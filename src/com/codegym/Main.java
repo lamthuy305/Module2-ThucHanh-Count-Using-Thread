33739442fff2c6cd4959e2e463d9b6b1309f7c81
lamthuy305 <lamthuy305@gmail.com>
2022-01-27 12:22:51 +0700

    done

---
+++ src/com/codegym/Main.java
@@ -0,0 +1,23 @@
+package com.codegym;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+            Count count = new Count();
+            try
+            {
+                while(count.getMyThread().isAlive())
+                {
+                    System.out.println("Main thread will be alive till the child thread is live");
+                    Thread.sleep(1500);
+                }
+            }
+            catch(InterruptedException e)
+            {
+                System.out.println("Main thread interrupted");
+            }
+            System.out.println("Main thread run is over" );
+    }
+}
